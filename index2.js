bd54c22937cd77cae20aa63eb1326793e0113a1d
Tannumishra <162034883+Tannumishra@users.noreply.github.com>
2024-04-14 19:00:21 +0530

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,10 @@
+var name="Tannu mishra"
+console.log("My Name is: ",name)
+
+
+var father_name="Mr sunil mishra"
+console.log("My Father Name is: ",father_name)
+
+
+var mother_name="Mrs guddi mishra"
+console.log("My Mother Name is: ",mother_name)
---
+++ index3.js
@@ -0,0 +1,4 @@
+var name="Tannu mishra"
+var age=21
+console.log("Name: ",name)
+console.log("age: ",age)
